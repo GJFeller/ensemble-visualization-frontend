bf15e67c50c2e4505d7082bf30e78a14b368a63e
Gustavo Jandt Feller <fellergustavo@gmail.com>
2024-01-14 20:06:06 -0300

    Testing event emitter to create charts in the future

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import './App.css';
 import DraggableWindow from './Components/DraggableWindow/DraggableWindow'
 import EnsembleSideBar from './Layout/EnsembleSideBar/EnsembleSideBar';
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { useEffect } from 'react';
+import EventEmitter from './utils/EventEmitter';
 
 function App() {
+  useEffect(() => {
+    const oncreateChart = (eventData)=>{
+      alert(eventData.chartType);
+    }
+
+    const listener = EventEmitter.addListener('createChart', oncreateChart);
+
+    return ()=>{
+      listener.remove();
+    }
+  },[])
   return (
     <div className="App" class="bg-slate-400">
       <PanelGroup autoSaveId="example" direction="horizontal">
-        <Panel minSize={20}>
+        <Panel minSize={20} maxSize={20}>
           <div class="min-h-screen bg-slate-400">
             <EnsembleSideBar></EnsembleSideBar>
           </div>
@@ -22,7 +35,7 @@ function App() {
           </div>
         </Panel>
         <PanelResizeHandle className="w-1 bg-black" />
-        <Panel minSize={20}>
+        <Panel minSize={20} maxSize={20}>
           <div class="min-h-screen bg-slate-400">
             right
           </div>
--- src/Layout/EnsembleSideBar/EnsembleSideBar.js
+++ src/Layout/EnsembleSideBar/EnsembleSideBar.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import EventEmitter from '../../utils/EventEmitter';
 
 export default class EnsembleSideBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.createChart = this.createChart.bind(this);
+  }
+
+  createChart() {
+    alert("Button clicked!");
+    EventEmitter.emit("createChart", {
+      chartType: "Test emitter"
+    });
+  }
 
   render() {
       return(
         <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create chart</button>
+            <button onClick={this.createChart} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create chart</button>
         </div>
       )
   }
