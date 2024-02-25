6ea33d724cc58d8585c5762f122e105dec196734
Gustavo Jandt Feller <fellergustavo@gmail.com>
2024-02-25 16:21:36 -0300

    Rotating arrow, still need to add animation

--- src/Components/CollapsableArrow.js
+++ src/Components/CollapsableArrow.js
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState } from "react"
+import arrow from '../Images/expand-arrow.svg'
+import emptyArrow from '../Images/expand-arrow-empty.svg'
 
 export default function CollapsableArrow({
   hasChildren,
@@ -13,21 +15,17 @@ export default function CollapsableArrow({
   };*/
 
   useEffect(() => {
-    const arrow = svg.current;
+    const arrowRef = svg.current;
     if(showChildren)
       setAngle(180);
     else
       setAngle(0)
-    arrow.setAttribute("transform", 'rotate('+angle+')');
+    arrowRef.setAttribute("transform", 'rotate('+angle+'deg)');
   }, [showChildren])
 
   return (
-    <svg  width="15pt" height="15pt" version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <g ref={svg}>
-        {hasChildren &&
-        <path transform="scale(4.1667)" d="m21 8.5003-9 9-9-9" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-        }
-      </g>
-    </svg> 
+    hasChildren ? 
+    <img ref={svg} src={arrow} style={showChildren ? {transform: 'rotate(180deg)'} : {}} alt="collapsable"/> :
+    <img ref={svg} src={emptyArrow} alt="empty"/>
   )
 }
