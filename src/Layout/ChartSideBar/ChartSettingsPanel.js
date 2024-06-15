2c4db380a6a515406cb8abbc676aa080a66de510
Gustavo Jandt Feller <fellergustavo@gmail.com>
2024-06-14 22:56:20 -0300

    Mid work commit to render chart options panel

--- src/Layout/ChartSideBar/ChartSettingsPanel.js
+++ src/Layout/ChartSideBar/ChartSettingsPanel.js
@@ -5,35 +5,53 @@ import * as ChartUtils from '../../utils/ChartUtils';
 export default function ChartSettingsPanel(
     currentChartSettings = undefined
 ) {
-    const [chartSettings, setChartSettings] = useState(currentChartSettings);
+    //const [chartSettings, setChartSettings] = useState(currentChartSettings);
 
-    useEffect( () => {
-        console.log("Teste");
-        setChartSettings(currentChartSettings);
-    }, [currentChartSettings]);
+    //useEffect( () => {
+    //    console.log("Teste");
+    //    setChartSettings(currentChartSettings);
+    //}, [currentChartSettings]);
 
     console.log(currentChartSettings);
+    console.log(ChartUtils.chartOptions)
+
+    const onChangeConvexHull = (e) => {
+
+    };
 
     return (
-        <div>
-            <p>Teste</p>
-        {chartSettings !== undefined ?
-            (
-                <div>
-                {chartSettings.chartType === ChartUtils.ChartType.DR &&
-                (
-                    <label>Dimensionality reduction technique: </label>
-                )
-                }
-                {chartSettings.chartType === ChartUtils.ChartType.TEMPORAL &&
-                (
-                    <label>Variable: </label>
-                )
-                }
-                </div>
-            ) :
-            (<p>undefined</p>)
+        <>
+        {currentChartSettings !== undefined &&
+           <div>
+           {currentChartSettings.currentChartSettings.chartType === ChartUtils.ChartType.DR &&
+               <>
+               <div>
+                  <label>Dimensionality reduction technique:</label>
+                  <select>
+                    {ChartUtils.chartOptions.drMethodList.length > 0 && ChartUtils.chartOptions.drMethodList.map((method) => {
+                        return (<option key={method} value={method}>{method}</option>)
+                    })}
+                  </select>
+               </div>
+               <div>
+                  <input 
+                    type="checkbox"
+                    id="convexHull"
+                    name="convexHull"
+                    value="convexHull"
+                    checked={currentChartSettings.currentChartSettings.drOptions.showConvexHull}
+                    onChange={onChangeConvexHull}
+                  /> Show convex hull
+               </div>
+               </>
+               
+           }
+           {currentChartSettings.currentChartSettings.chartType === ChartUtils.ChartType.TEMPORAL &&
+               <label>Variable:</label>
+           }
+           </div>
+
         }
-        </div>
+        </>
     );
 }
--- src/Layout/ChartSideBar/ChartSideBar.js
+++ src/Layout/ChartSideBar/ChartSideBar.js
@@ -5,14 +5,17 @@ import ChartSettingsPanel from './ChartSettingsPanel';
 export default function ChartSideBar({
   currentChartSettings = undefined
 }) {
+    console.log(currentChartSettings);
     return (
         <div className='border-1 rounded-md m-1'>
           <Accordion title="Selected Simulations"/>
-          <Accordion title="Chart Settings for ">
+          {currentChartSettings !== undefined &&
+          <Accordion title={"Chart Settings for "+currentChartSettings.chartTitle}>
             <ChartSettingsPanel
               currentChartSettings={currentChartSettings}
             />
           </Accordion>
+          }
         </div>
     );
 }
