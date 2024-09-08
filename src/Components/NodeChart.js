c3ec81c33422502523050922e905d2607e61ebc9
Gustavo Jandt Feller <Gustavo Jandt Feller>
2024-09-08 17:08:30 -0300

    Resize from the chart working. Obs.: It gives error when Grammarly is on

--- src/Components/NodeChart.js
+++ src/Components/NodeChart.js
@@ -17,6 +17,8 @@ import "@xyflow/react/dist/base.css";
 const NodeChart = ({ data }) => {
   let chartSettings = data.chartSettings;
   const closeWindow = data.closeWindow;
+  const container = useRef(null);
+  const resizible = useRef(null);
   const windowBodyId = "window-body-" + chartSettings.chartId;
 
   console.log(chartSettings);
@@ -55,8 +57,81 @@ const NodeChart = ({ data }) => {
 
   return (
     <>
-      <NodeResizer minWidth={100} minHeight={100} />
-      <div className="p-2">{currentChartSettings.chartTitle}</div>
+      <NodeResizer
+        minWidth={200}
+        minHeight={200}
+        onResizeEnd={() =>
+          ChartRender.drawChart(
+            currentChartSettings.chartId,
+            currentChartSettings,
+          )
+        }
+      />
+      <div
+        id={"viz-" + chartSettings.chartId}
+        ref={resizible}
+        className="flex flex-col items-stretch min-w-full min-h-full max-w-full max-h-full border-2 overflow-clip"
+      >
+        <div className="handle justify-items-stretch">
+          <div
+            id="header"
+            className="bg-gray-300 px-2 h-16 flex flex-row space-x-2 rounded"
+          >
+            <div className="grow place-self-center">
+              <h2 className="text-center">{currentChartSettings.chartTitle}</h2>
+            </div>
+            <div className="place-self-center flex justify-end space-x-2">
+              <button
+                className="border-2 border-black rounded-lg p-1"
+                onClick={openSettings}
+              >
+                <img
+                  src={parentIcon}
+                  width="24"
+                  height="24"
+                  alt="set parent window"
+                />
+              </button>
+              <button
+                className="border-2 border-black rounded-lg p-1"
+                onClick={openSettings}
+              >
+                <img
+                  src={optionsIcon}
+                  width="24"
+                  height="24"
+                  alt="chart options window"
+                />
+              </button>
+              <button
+                className="border-2 border-black rounded-lg p-1"
+                onClick={closeWindowPressed}
+              >
+                <img
+                  src={closeIcon}
+                  width="24"
+                  height="24"
+                  alt="close window"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          id={windowBodyId}
+          className="flex items-center flex-auto max-w-full max-h-full overflow-auto"
+          ref={container}
+        >
+          <svg id={currentChartSettings.chartId}></svg>
+        </div>
+      </div>
+      <ModalChartSettings
+        isOpen={isOpenModal}
+        setIsOpenModal={() => setIsOpenModal(!isOpenModal)}
+        title={modalTitle}
+        chartSettings={currentChartSettings}
+        saveChartSettings={saveChartSettings}
+      ></ModalChartSettings>
     </>
   );
 };
